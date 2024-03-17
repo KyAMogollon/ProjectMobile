ad0c5af989b4c9de05732f1d1e35eddcb610691d
KyAMogollon <adhair.mogollon@tecsup.edu.pe>
2024-03-16 22:43:59 -0500

    SwipeAndFixDoubleTap

--- Assets/Scripts/TouchController.cs
+++ Assets/Scripts/TouchController.cs
@@ -5,15 +5,23 @@ using static UnityEditor.PlayerSettings;
 public class TouchController : MonoBehaviour
 {
     [SerializeField] GameObject[] objeto;
+    [SerializeField] GameObject almacenObjetos;
     private Camera camera;
     private int index=0;
-    private float lastTapTime;
-    private float doubleTapTime = 0.3f;
-    bool doubleTapDetected=false;
+
+
+
+    public float swipeThreshold = 100f; // Umbral del swipe
+
+    private Vector2 startTouchPosition;
+    private Vector2 endTouchPosition;
+    private TrailRenderer trailRenderer; // Trail Renderer activo
+
     // Start is called before the first frame update
     void Start()
     {
         camera=FindObjectOfType<Camera>();
+        trailRenderer=GetComponent<TrailRenderer>();
     }
 
     // Update is called once per frame
@@ -22,38 +30,55 @@ public class TouchController : MonoBehaviour
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-
             if (touch.phase == TouchPhase.Began)
             {
-                if (Time.time - lastTapTime < doubleTapTime)
-                {
-                    Debug.Log("DobleToque");
-                    doubleTapDetected = true;
-                }
-
-                else
-                {
-                    Vector2 pos = Camera.main.ScreenToWorldPoint(touch.position);
-                    if (!doubleTapDetected)
-                    {
-                        RaycastDetected(pos);
-
-                    }
-                }
-                lastTapTime = Time.time;
-                StartCoroutine(ResetDoubleTap());
+                RaycastDetected(touch);
             }
             Vector2 pos2=Camera.main.ScreenToWorldPoint(touch.position);
+            //Para mover la figura
             if(touch.phase == TouchPhase.Moved)
             {
                 MoveFigure(pos2);
             }
         }
+        DetectSwipe();
     }
-    IEnumerator ResetDoubleTap()
+    void DetectSwipe()
     {
-        yield return new WaitForSeconds(0.5f); 
-        doubleTapDetected = false;
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                startTouchPosition = touch.position;
+            }
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                endTouchPosition = touch.position;
+
+                float swipeMagnitude = Vector2.Distance(startTouchPosition, endTouchPosition);
+
+                if (swipeMagnitude > swipeThreshold)
+                {
+                    Vector2 pos = Camera.main.ScreenToWorldPoint(touch.position);
+                    RaycastDestroy(pos);
+                }
+            }
+
+        }
+    }
+    void RaycastDestroy(Vector2 pos2)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(pos2, Vector2.zero);
+        if (hit.collider == null)
+        {
+            for (int i = 0; i < almacenObjetos.transform.childCount; i++)
+            {
+                Destroy(almacenObjetos.transform.GetChild(i).gameObject);
+            }
+        }
     }
     void MoveFigure(Vector2 pos2)
     {
@@ -63,12 +88,20 @@ public class TouchController : MonoBehaviour
             hit.collider.gameObject.transform.position = new Vector2(pos2.x, pos2.y);
         }
     }
-    void RaycastDetected(Vector2 pos)
+    void RaycastDetected(Touch touch)
     {
-        RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
-        if (hit.collider ==null)
+        Vector2 pos1 = Camera.main.ScreenToWorldPoint(touch.position);
+        RaycastHit2D hit = Physics2D.Raycast(pos1, Vector2.zero);
+        if (hit.collider != null)
         {
-            Instantiate(objeto[index], pos, Quaternion.identity);
+            if (touch.tapCount == 2)
+            {
+                Destroy (hit.collider.gameObject);
+            }
+        }
+        else
+        {
+            Instantiate(objeto[index], pos1, Quaternion.identity, almacenObjetos.transform);
         }
     }
     public void CircleFigure()
